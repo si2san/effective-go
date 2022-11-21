a1dca8fe4bef8a663913e80070c4edcf29e0b74c
Si Thu San <sithusan@aceplussolutions.com>
2022-11-21 23:19:43 +0630

    Generic With Constrait

--- generics/main.go
+++ generics/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type Number interface {
+	int64 | float64
+}
+
 func main() {
 	ints := map[string]int64{
 		"first":  34,
@@ -16,6 +20,8 @@ func main() {
 	fmt.Printf("Non-Generic Sums: %v, and %v\n", SumInt(ints), SumFloats(floats))
 	// go compiler includes type type inference, So can drop type arguments.
 	fmt.Printf("Generic Sums:%v, and %v\n", SumIntsOrFloat(ints), SumIntsOrFloat(floats))
+	fmt.Printf("Generic with constraints:%v, and %v\n", SumNumbers(ints), SumNumbers(floats))
+
 }
 
 func SumInt(m map[string]int64) int64 {
@@ -38,6 +44,15 @@ func SumFloats(m map[string]float64) float64 {
 
 // comparable constraint is predeclared in Go.
 func SumIntsOrFloat[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func SumNumbers[K comparable, V Number](m map[K]V) V {
 	var s V
 
 	for _, v := range m {
