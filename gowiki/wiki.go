b5c9561255f957e74ddba7a6159cacef45189fa2
Si Thu San <sithusan@aceplussolutions.com>
2022-11-23 23:28:21 +0630

    Go Wiki View Page

---
+++ gowiki/wiki.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, error := os.ReadFile(filename)
+
+	if error != nil {
+		return nil, error
+	}
+
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div", p.Title, p.Body)
+}
